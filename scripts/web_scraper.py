99e3d7fa3d97416d4d0e752da7d552b485e25836
John Tran <johnttra@usc.edu>
2025-09-02 01:24:32 -0700

    Finished 2.3 finished web_scraper.py and added web_data.html to raw_data)

--- scripts/web_scraper.py
+++ scripts/web_scraper.py
@@ -1,8 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.cnbc.com/world/?region=world'
-response = requests.get(url).text
-soup = BeautifulSoup(response, 'html.parser')
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
-for link in soup.find_all
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+
+url = 'https://www.cnbc.com/world/?region=world'
+driver = webdriver.Chrome()
+driver.get(url)
+WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+    (By.CLASS_NAME, 'MarketCard-row')))
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+market_banner = soup.find('div', class_='MarketsBanner-marketData').prettify()
+latest_news = soup.find('ul', class_='LatestNews-list').prettify()
+
+with open('../data/raw_data/web_data.html', 'w', encoding='utf-8') as f:
+    f.write(str(market_banner))
+    f.write(str(latest_news))
